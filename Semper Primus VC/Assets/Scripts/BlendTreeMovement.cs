d2a4af9d7acfc853ff6080b3d510ec050e415663
Hesham Maher <etch7313@gmail.com>
2020-08-05 03:18:00 +0200

    Blend Tree Done

---
+++ Semper Primus VC/Assets/Scripts/BlendTreeMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlendTreeMovement : MonoBehaviour
+{
+   public Animator MoveANIM;
+    float X;
+    float Y;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        X = Input.GetAxis("Horizontal");
+        Y = Input.GetAxis("Vertical");
+        MoveANIM.SetFloat("x", X);
+        MoveANIM.SetFloat("y", Y);
+    }
+}
